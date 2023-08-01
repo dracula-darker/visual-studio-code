63a79a231fcc1ca5b7f902ba9945d1bcf5bbaf37
Carve <cj@cjratliff.com>
2023-08-01 08:42:21 -0400

    More updates

--- scripts/build.js
+++ scripts/build.js
@@ -13,11 +13,11 @@ module.exports = async () => {
 
     return Promise.all([
         fs.promises.writeFile(
-            path.join(THEME_DIR, 'dracula.json'),
+            path.join(THEME_DIR, 'dracula-darker.json'),
             JSON.stringify(base, null, 4)
         ),
         fs.promises.writeFile(
-            path.join(THEME_DIR, 'dracula-soft.json'),
+            path.join(THEME_DIR, 'dracula-darker-soft.json'),
             JSON.stringify(soft, null, 4)
         ),
     ]);
